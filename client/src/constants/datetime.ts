c756e037d05d482229a7a190c2db02f912ee791f
Jonathan <Jonathan@iMac.local>
2022-08-14 10:40:41 -0400

    editing date timezone

--- client/src/constants/datetime.ts
+++ client/src/constants/datetime.ts
@@ -1,6 +1,6 @@
-import moment from "moment";
+import moment from "moment-timezone";
 
-export const currentDayYearHour = moment(Date.now()).format(
-  "MMM Do YYYY, hh:mm A"
-);
+export const currentDayYearHour = moment(Date.now())
+  .tz("America/Toronto")
+  .format("MMM Do YYYY, hh:mm A");
 export const currentDayYear = moment(Date.now()).format("MMM Do YYYY");
